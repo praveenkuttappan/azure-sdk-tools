bc2682f57bba167bd1aef31b3ab54d5ca3ac7358
praveenkuttappan <praveenkuttappan@gmail.com>
2024-04-13 01:11:28 -0400

    Authenticate requests to DevOps using managed identity

--- src/dotnet/APIView/APIViewWeb/Repositories/DevopsArtifactRepository.cs
+++ src/dotnet/APIView/APIViewWeb/Repositories/DevopsArtifactRepository.cs
@@ -1,112 +1,106 @@
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.TeamFoundation.Build.WebApi;
 using Microsoft.TeamFoundation.Core.WebApi;
-using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
 using Newtonsoft.Json;
-using Octokit;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading.Tasks;
+using Polly;
+using Azure.Core;
+using Azure.Identity;
+using System.Threading;
+using Microsoft.VisualStudio.Services.Client;
 
 namespace APIViewWeb.Repositories
 {
     public class DevopsArtifactRepository : IDevopsArtifactRepository
     {
-        private readonly HttpClient _devopsClient;
         private readonly IConfiguration _configuration;
-        private readonly string _devopsAccessToken;
         private readonly string _hostUrl;
         private readonly TelemetryClient _telemetryClient;
 
         public DevopsArtifactRepository(IConfiguration configuration, TelemetryClient telemetryClient)
         {
             _configuration = configuration;
-            _devopsAccessToken = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", _configuration["Azure-Devops-PAT"])));
             _hostUrl = _configuration["APIVIew-Host-Url"];
             _telemetryClient = telemetryClient;
-
-            _devopsClient = new HttpClient();
-            _devopsClient.DefaultRequestHeaders.Accept.Clear();
-            _devopsClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _devopsClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _devopsAccessToken);
         }
 
-        public async Task<Stream> DownloadPackageArtifact(string repoName, string buildId, string artifactName, string filePath, string project, string format= "file")
+        public async Task<Stream> DownloadPackageArtifact(string repoName, string buildId, string artifactName, string filePath, string project, string format = "file")
         {
-            var downloadUrl = await GetDownloadArtifactUrl(repoName, buildId, artifactName, project);
-            if (!string.IsNullOrEmpty(downloadUrl))
+            var downloadUrl = await getDownloadArtifactUrl(buildId, artifactName, project);
+            if (string.IsNullOrEmpty(downloadUrl))
             {
-                if(!string.IsNullOrEmpty(filePath))
+                throw new Exception(string.Format("Failed to get download url for artifact {0} in build {1} in project {2}", artifactName, buildId, project));
+            }
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                if (!filePath.StartsWith("/"))
                 {
-                    if (!filePath.StartsWith("/"))
-                    {
-                        filePath = "/" + filePath;
-                    }
-                    downloadUrl = downloadUrl.Split("?")[0] + "?format=" + format + "&subPath=" + filePath;
+                    filePath = "/" + filePath;
                 }
-
-                var downloadResp = await GetFromDevopsAsync(downloadUrl);
-                downloadResp.EnsureSuccessStatusCode();
-                return await downloadResp.Content.ReadAsStreamAsync();
+                downloadUrl = downloadUrl.Split("?")[0] + "?format=" + format + "&subPath=" + filePath;
             }
-            return null;
+
+            HttpResponseMessage downloadResp = await GetFromDevOpsAsync(downloadUrl);
+            downloadResp.EnsureSuccessStatusCode();
+            return await downloadResp.Content.ReadAsStreamAsync();
         }
 
-        private async Task<HttpResponseMessage> GetFromDevopsAsync(string request)
+        private static async Task<string> getDownloadArtifactUrl(string buildId, string artifactName, string project)
         {
-            var downloadResp = await _devopsClient.GetAsync(request);
-            int count = 0;
-            int[] waitTimes = new int[] { 0, 1, 2, 4, 8, 16, 32, 64, 128, 256 };
-            while ((downloadResp.StatusCode == HttpStatusCode.TooManyRequests || downloadResp.StatusCode == HttpStatusCode.BadRequest) && count < waitTimes.Length)
+            var connection = await CreateVssConnection();
+            var buildClient = connection.GetClient<BuildHttpClient>();
+            var artifact = await buildClient.GetArtifactAsync(project, int.Parse(buildId), artifactName);
+            return artifact?.Resource?.DownloadUrl;
+        }
+
+        private static async Task<VssConnection> CreateVssConnection()
+        {
+            var accessToken = await getAccessToken();
+            var token = new VssAadToken("Bearer", accessToken);
+            return new VssConnection(new Uri("https://dev.azure.com/azure-sdk/"), new VssAadCredential(token));
+        }
+
+        private static async Task<string> getAccessToken()
+        {
+            // APIView deployed instances uses managed identity to authenticate requests to Azure DevOps.
+            // For local testing, VS will use developer credentials to create token
+            var credential = new DefaultAzureCredential();
+            var tokenRequestContext = new TokenRequestContext(VssAadSettings.DefaultScopes);
+            var token = await credential.GetTokenAsync(tokenRequestContext, CancellationToken.None);
+            return token.Token;
+        }
+
+        private static async Task<HttpResponseMessage> GetFromDevOpsAsync(string request)
+        {
+            var httpClient = new HttpClient();
+            var maxRetryAttempts = 10;
+            var pauseBetweenFailures = TimeSpan.FromSeconds(2);
+
+            var retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(maxRetryAttempts, i => pauseBetweenFailures);
+
+            HttpResponseMessage downloadResp = null;
+            await retryPolicy.ExecuteAsync(async () =>
             {
-                _telemetryClient.TrackTrace($"Download request from devops artifact is either throttled or flaky, waiting {waitTimes[count]} seconds before retrying, Retry count: {count}");
-                await Task.Delay(TimeSpan.FromSeconds(waitTimes[count]));
-                downloadResp = await _devopsClient.GetAsync(request);
-                count++;
-            }
+                downloadResp = await httpClient.GetAsync(request);
+            });
             return downloadResp;
-        }
-
-        private async Task<string> GetDownloadArtifactUrl(string repoName, string buildId, string artifactName, string project)
-        {
-            var artifactGetReq = GetArtifactRestAPIForRepo(repoName).Replace("{buildId}", buildId).Replace("{artifactName}", artifactName).Replace("{project}", project);
-            var response = await GetFromDevopsAsync(artifactGetReq);
-            response.EnsureSuccessStatusCode();
-            var buildResource = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-            if (buildResource == null)
-            {
-                return null;
-            }
-            return buildResource.RootElement.GetProperty("resource").GetProperty("downloadUrl").GetString();
-        }
-
-        private string GetArtifactRestAPIForRepo(string repoName)
-        {
-            var downloadArtifactRestApi = _configuration["download-artifact-rest-api-for-" + repoName];
-            if (downloadArtifactRestApi == null)
-            {
-                downloadArtifactRestApi = _configuration["download-artifact-rest-api"];
-            }
-            return downloadArtifactRestApi;
         }
 
         public async Task RunPipeline(string pipelineName, string reviewDetails, string originalStorageUrl)
         {
-            //Create dictionary of all required parametes to run tools - generate-<language>-apireview pipeline in azure devops
+            //Create dictionary of all required parameters to run tools - generate-<language>-apireview pipeline in azure devops
             var reviewDetailsDict = new Dictionary<string, string> { { "Reviews", reviewDetails }, { "APIViewUrl", _hostUrl }, { "StorageContainerUrl", originalStorageUrl } };
-            var devOpsCreds = new VssBasicCredential("nobody", _configuration["Azure-Devops-PAT"]);
-            var devOpsConnection = new VssConnection(new Uri($"https://dev.azure.com/azure-sdk/"), devOpsCreds);
+            var devOpsConnection = await CreateVssConnection();
             string projectName = _configuration["Azure-Devops-internal-project"] ?? "internal";
 
             BuildHttpClient buildClient = await devOpsConnection.GetClientAsync<BuildHttpClient>();
@@ -118,8 +112,8 @@ namespace APIViewWeb.Repositories
             {
                 throw new Exception(string.Format("Azure Devops pipeline is not found with name {0}. Please recheck and ensure pipeline exists with this name", updatedPipelineName));
             }
-            
-            var definition = await buildClient.GetDefinitionAsync(projectName, definitionId);            
+
+            var definition = await buildClient.GetDefinitionAsync(projectName, definitionId);
             var project = await projectClient.GetProject(projectName);
             await buildClient.QueueBuildAsync(new Build()
             {
@@ -130,7 +124,7 @@ namespace APIViewWeb.Repositories
         }
 
         private async Task<int> GetPipelineId(string pipelineName, BuildHttpClient client, string projectName)
-        {          
+        {
             var pipelines = await client.GetFullDefinitionsAsync2(project: projectName);
             if (pipelines != null)
             {
